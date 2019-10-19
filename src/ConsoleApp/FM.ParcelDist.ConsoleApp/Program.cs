13dcf88edef77eaa817388f13b5db034661fb44c
AliReza <alireza.es@gmail.com>
2019-10-19 11:38:10 +0330

    Complete Console App

--- src/ConsoleApp/FM.ParcelDist.ConsoleApp/Program.cs
+++ src/ConsoleApp/FM.ParcelDist.ConsoleApp/Program.cs
@@ -1,12 +1,58 @@
 ﻿using System;
+using System.Collections.Generic;
+using System.Threading;
+using FM.ParcelDist.Core.Domain;
+using FM.ParcelDist.Core.Services;
+using FM.ParcelDist.Core.Utility;
 
 namespace FM.ParcelDist.ConsoleApp
 {
     class Program
     {
+        const string XmlFilePath = "Files/Container.xml";
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            var container = XmlParser.LoadXml<ParcelContainer>(XmlFilePath);
+            var defaultOrganization = CreateOrganization();
+
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine($"************** {container.Parcels.Count} Parcels is ready to process **************");
+            Console.WriteLine("***********************************************************");
+            Console.WriteLine();
+            Console.WriteLine();
+
+            Process(defaultOrganization, container);
+
+            Console.WriteLine();
+            Console.WriteLine("*************************     Done!    *************************");
+            Console.WriteLine();
+
+        }
+
+        private static void Process(Organization organization, ParcelContainer container)
+        {
+            var service = new ParcelDistributionService();
+
+            foreach (var parcel in container.Parcels)
+            {
+                service.Send(organization, parcel);
+
+                Thread.Sleep(3000);//Just for simulation
+            }
+        }
+        private static Organization CreateOrganization()
+        {
+            return new Organization
+            {
+                Departments = new List<Department>
+                {
+                    new InsuranceDepartment(),
+                    new EmailDepartment(),
+                    new RegularDepartment(),
+                    new HeavyDepartment(),
+                    new SampleAddedDepartment()
+                }
+            };
         }
     }
 }
